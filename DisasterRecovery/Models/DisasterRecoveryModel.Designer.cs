a5be73b4b24a532c22367b714aab93bd786200bf
Jose Antonio Valencia Delgado <valencajoseantonio@hotmail.com>
2020-06-18 13:00:57 -0400

    no changes

--- DisasterRecovery/Models/DisasterRecoveryModel.Designer.cs
+++ DisasterRecovery/Models/DisasterRecoveryModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Kelvin\source\repos\DisasterRecovery\DisasterRecovery\Models\DisasterRecoveryModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\zafir\Documents\summitworks\project Team 4\git repository\DisasterRecovery\Models\DisasterRecoveryModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
