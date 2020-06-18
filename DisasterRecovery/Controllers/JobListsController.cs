44918f90be8f98fc8d88a40fab5acfc354ba55d6
Jose Antonio Valencia Delgado <valencajoseantonio@hotmail.com>
2020-06-18 13:29:44 -0400

    test2

--- DisasterRecovery/Controllers/JobListsController.cs
+++ DisasterRecovery/Controllers/JobListsController.cs
@@ -7,7 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using DisasterRecovery.Models;
-
+// test
 namespace DisasterRecovery.Controllers
 {
     public class JobListsController : Controller
