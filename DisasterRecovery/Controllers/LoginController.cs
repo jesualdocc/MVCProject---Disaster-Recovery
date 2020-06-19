6886313809095ecf0d000a820406dd59cf755725
Kelvin Reyes <ingkelvinreyes@gmail.com>
2020-06-18 23:46:56 -0400

    Backend Design, Login First Work

---
+++ DisasterRecovery/Controllers/LoginController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DisasterRecovery.Models;
+
+namespace DisasterRecovery.Controllers
+{
+    public class LoginController : Controller
+    {
+        private DisasterRecoveryEntities db = new DisasterRecoveryEntities();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            var users = db.Users.Include(u => u.SubContractor);
+            return View();
+        }
+
+        // GET: Login/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Login/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdSubContractor = new SelectList(db.SubContractors, "IdSubContractor", "SubContractorName");
+            return View();
+        }
+
+        // POST: Login/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdUser,UserName,FirstName,LastName,Email,UserPassWord,UserStatus,IsAdm,IdSubContractor")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdSubContractor = new SelectList(db.SubContractors, "IdSubContractor", "SubContractorName", user.IdSubContractor);
+            return View(user);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
