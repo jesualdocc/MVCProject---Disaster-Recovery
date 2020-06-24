2c4b9827ce4539d79baf52e3ee830b721941abbb
Kelvin Reyes <ingkelvinreyes@gmail.com>
2020-06-24 10:16:56 -0400

    Adding Autorization

---
+++ DisasterRecovery/Infrastructure/CustomAuthorizeAttribute.cs
@@ -0,0 +1,44 @@
+﻿using DisasterRecovery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+namespace DisasterRecovery.Infrastructure
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        private readonly string[] allowedroles;
+        public CustomAuthorizeAttribute(params string[] roles)
+        {
+            this.allowedroles = roles;
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+            var userId = Convert.ToString(httpContext.Session["LogeUserID"]);
+            if (!string.IsNullOrEmpty(userId))
+                using (var context = new DisasterRecoveryEntities())
+                {
+                    var userRole = httpContext.Session["LogeUserRole"];
+                    {
+                        return true;
+                    }
+                }
+
+
+            return authorize;
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectToRouteResult(
+               new RouteValueDictionary
+               {
+                    { "controller", "Home" },
+                    { "action", "UnAuthorized" }
+               });
+        }
+    }
+}
